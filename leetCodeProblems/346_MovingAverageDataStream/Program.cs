f8c796b6c86b2a9cc908bb7678358cc0aa232f01
Rudson Souza <rudsonsouza@Rudsons-MacBook-Air.local>
2022-07-02 19:36:41 -0300

    346 - Moving Average from Data Stream

---
+++ leetCodeProblems/346_MovingAverageDataStream/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _346_MovingAverageDataStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MovingAverage obj = new MovingAverage(3);
+            double param_1 = obj.Next(1);
+            double param_2 = obj.Next(10);
+            double param_3 = obj.Next(3);
+            double param_4 = obj.Next(5);
+        }
+    }
+
+    public class MovingAverage
+    {
+        public Queue<double> Window;
+        public int Size;
+        public double CurSum;
+
+        public MovingAverage(int size)
+        {
+            Window = new Queue<double>();
+            Size = size;
+            CurSum = 0;
+        }
+
+        public double Next(int val)
+        {
+            Window.Enqueue(val);
+            CurSum += val;
+
+            if (Window.Count > Size)
+            {
+                CurSum = CurSum - Window.Dequeue();
+                return CurSum / Size;
+            }
+            else
+            {
+                return CurSum / Window.Count;
+            }
+        }
+    }
+}
+
