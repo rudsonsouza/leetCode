b7b77de24ba217b4ec670a63c37d7afe8e7a5bdd
Rudson Souza <rudsonsouza@Rudsons-MacBook-Air.local>
2022-07-03 11:30:01 -0300

    359_LoggerRateLimiter

---
+++ leetCodeProblems/359_LoggerRateLimiter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _359_LoggerRateLimiter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Logger
+    {
+        Dictionary<string, int> msgTimeStampTracker;
+
+        public Logger()
+        {
+            this.msgTimeStampTracker = new Dictionary<string, int>();
+        }
+
+        public bool ShouldPrintMessage(int timestamp, string message)
+        {
+            if (!msgTimeStampTracker.ContainsKey(message))
+            {
+                msgTimeStampTracker.Add(message, timestamp + 10);
+                return true;
+            }
+            else
+            {
+                if (msgTimeStampTracker[message] <= timestamp)
+                {
+                    msgTimeStampTracker[message] = timestamp + 10;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
+
