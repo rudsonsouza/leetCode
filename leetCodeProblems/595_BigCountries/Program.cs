d56f20cd6253e1bd613fa3e5c2254838712b8f11
Rudson Souza <rudsonsouza@Rudsons-MacBook-Air.local>
2022-07-03 11:41:19 -0300

    595_BigCountries

---
+++ leetCodeProblems/595_BigCountries/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _595_BigCountries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    /* SELECT name, population, area
+     *   FROM world
+     *  WHERE area >= 3000000
+     *    AND population >= 25000000;
+     */
+}
+
